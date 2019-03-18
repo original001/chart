1bbea2d1a91592f47c4948562e68afb6d017da6b
Vladimir Tolstikov <wertyxa@bk.ru>
2019-03-18 21:36:43 +0500

    realized moving

--- src/slider.ts
+++ src/slider.ts
@@ -3,37 +3,44 @@ import { ComponentType, componentMixin, createElement } from "./reconciler";
 import { CHART_WIDTH, CHART_HEIGHT } from "./constant";
 
 const DRAG_HANDLER_NAME = "onSliderDrag";
-const DRAG_RESIZE_HANDLER_NAME = "onSliderDragResize";
+const DRAG_RESIZE_RIGHT_HANDLER_NAME = "onSliderDragResizeRight";
+const DRAG_RESIZE_LEFT_HANDLER_NAME = "onSliderDragResizeLeft";
 const START_DRAG_HANDLER_NAME = "onSliderStartDrag";
 
 export const Slider: ComponentType = () => ({
   ...componentMixin(),
   state: {
     left: 0,
-    right: CHART_WIDTH - 500
+    right: CHART_WIDTH
   },
   didMount() {
     let beginClientX;
     let beginLeft;
     let beginRight;
     let compensation;
+    let compensationRight;
     window[DRAG_HANDLER_NAME] = createRaf(e => {
-      // console.log(e.clientX)
       this.send({ type: "updatePos", payload: e.clientX - compensation });
     });
-    window[DRAG_RESIZE_HANDLER_NAME] = createRaf((e: DragEvent) => {
-      // console.log(e.clientX)
-      // e.stopPropagation();
+    window[DRAG_RESIZE_RIGHT_HANDLER_NAME] = createRaf((e: DragEvent) => {
       this.send({
-        type: "updateWidth",
-        payload: e.clientX - beginClientX + beginLeft
+        type: "updateRight",
+        payload: e.clientX - compensationRight
+      });
+    });
+    window[DRAG_RESIZE_LEFT_HANDLER_NAME] = createRaf((e: DragEvent) => {
+      this.send({
+        type: "updateLeft",
+        payload: e.clientX - compensation
       });
     });
     window[START_DRAG_HANDLER_NAME] = (e: DragEvent) => {
       // e.preventDefault();
       beginClientX = e.clientX;
       beginLeft = this.state.left;
+      beginRight = this.state.right;
       compensation = 8 + (beginClientX - beginLeft);
+      compensationRight = 8 - (beginRight - beginClientX);
       let dragImage = document.createElement("img");
       dragImage.src =
         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
@@ -49,8 +56,12 @@ export const Slider: ComponentType = () => ({
         else if (payload + size > CHART_WIDTH)
           return { left: CHART_WIDTH - size, right: CHART_WIDTH };
         return { left: payload, right: size + payload };
-      case "updateWidth":
+      case "updateRight":
+        if (payload > CHART_WIDTH) return { ...state, right: CHART_WIDTH };
         return { ...state, right: payload };
+      case "updateLeft":
+        if (payload < 0) return { ...state, left: 0 };
+        return { ...state, left: payload };
     }
   },
   render: (props, state) => {
@@ -61,32 +72,26 @@ export const Slider: ComponentType = () => ({
         style: `left: ${state.left - CHART_WIDTH}px; right: ${-state.right}px;`
       },
       [
-        createElement(
-          "div",
-          {
-            class: "slider"
-          },
-          [
-            createElement("div", {
-              class: "sliderEdge sliderEdgeLeft",
-              ondrag: `${DRAG_RESIZE_HANDLER_NAME}(event)`,
-              ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
-              draggable: "true"
-            }),
-            createElement("div", {
-              class: "sliderCenter",
-              ondrag: `${DRAG_HANDLER_NAME}(event)`,
-              ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
-              draggable: "true"
-            }),
-            createElement("div", {
-              class: "sliderEdge sliderEdgeRight",
-              ondrag: `${DRAG_RESIZE_HANDLER_NAME}(event)`,
-              ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
-              draggable: "true"
-            })
-          ]
-        )
+        createElement("div", { class: "slider" }, [
+          createElement("div", {
+            class: "sliderEdge sliderEdgeLeft",
+            ondrag: `${DRAG_RESIZE_LEFT_HANDLER_NAME}(event)`,
+            ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
+            draggable: "true"
+          }),
+          createElement("div", {
+            class: "sliderCenter",
+            ondrag: `${DRAG_HANDLER_NAME}(event)`,
+            ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
+            draggable: "true"
+          }),
+          createElement("div", {
+            class: "sliderEdge sliderEdgeRight",
+            ondrag: `${DRAG_RESIZE_RIGHT_HANDLER_NAME}(event)`,
+            ondragstart: `${START_DRAG_HANDLER_NAME}(event)`,
+            draggable: "true"
+          })
+        ])
       ]
     );
   }
