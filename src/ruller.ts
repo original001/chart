0ed7a5057710dfedbce610941cda67fc5160e66f
Vladimir Tolstikov <wertyxa@bk.ru>
2019-04-02 22:51:54 +0500

    make great animations

--- src/ruller.ts
+++ src/ruller.ts
@@ -1,114 +1,55 @@
 import { ComponentType, componentMixin, createElement } from "./reconciler";
 import { CHART_HEIGHT } from "./constant";
+import { TransitionGroup } from "./labels";
+import { Transition } from "./transition";
 
-
-const ruler = (y: number, label: string) =>
-  createElement("g", {}, [
+const ruler = (projectY: number, y: number, scale: number, status: string) =>
+  createElement("g", { key: y }, [
     createElement("line", {
       x1: "0",
-      y1: CHART_HEIGHT - y + "",
+      y1: projectY + "",
       x2: "100%",
-      y2: CHART_HEIGHT - y + "",
+      y2: projectY + "",
       class: "r-line"
     }),
     createElement(
       "text",
       {
         x: 0,
-        y: CHART_HEIGHT - y - 5 + "",
-        class: "r-text"
+        y: projectY - 5 + "",
+        class: `${status} transition translate r-text`,
+        style: `transform: scaleY(${1 /
+          scale}); transform-origin: 0 ${projectY - 5}px`
       },
-      label
+      y.toString()
     )
   ]);
-type Status = "entering" | "entered" | "exiting" | "exited";
-interface State {
-  nextValues: number[];
-  nextScale: number;
-  values: number[];
-  scale: number;
-  status: Status;
-}
-    let inProccess = false;
+
 export const TransitionRuller: ComponentType = () => ({
   ...componentMixin(),
-  state: {
-    nextValues: null,
-    nextScale: null,
-    values: null,
-    scale: null,
-    status: null
-  } as State,
-  getDeriviedStateFromProps(props, prevState: State): State {
-    if (prevState.values === null) {
-      return {
-        ...prevState,
-        values: props.values,
-        scale: props.scale,
-        status: "entered"
-      };
-    }
-    if (props.values[2] !== prevState.values[2] || props.values.length !== prevState.values.length) {
-      return {
-        values: prevState.values,
-        scale: prevState.scale,
-        nextValues: props.values,
-        nextScale: props.scale,
-        status: "exiting"
-      };
-    }
-    return prevState;
-  },
-  didUpdate() {
-    if (inProccess) return;
-
-    const prevState = this.state as State;
-    if (prevState.status === "exiting") {
-      inProccess = true;
-      setTimeout(() => {
-        inProccess = false;
-        this.send({ type: "update" });
-      }, 200);
-    }
-    if (prevState.status === "exited") {
-      this.send({ type: "show" });
-    }
-    if (prevState.status === "entering") {
-      inProccess = true;
-      setTimeout(() => {
-        inProccess = false;
-        this.send({ type: "entered" });
-      }, 20);
-    }
-  },
-  render: (props, state: State) => {
-    return createElement("g", {}, state.status === 'exited' ? null : [
-      createElement(
-        "g",
-        { class: state.status + " transition translate", secondValue: state.values[1] },
-        state.values.slice(0,-1).map(y => ruler((y - state.values[0]) * state.scale, y.toString()))
+  render: (props, state) => {
+    return createElement(
+      TransitionGroup,
+      {
+        wrapper: children =>
+          createElement(
+            "g",
+            {
+              class: "transition translate",
+              style: `transform: scaleY(${
+                props.scale
+              }); transform-origin: 0 ${CHART_HEIGHT}px`
+            },
+            children
+          )
+      },
+      props.values.slice(0, -1).map(y =>
+        createElement(Transition, {
+          key: y,
+          children: status =>
+            ruler(CHART_HEIGHT - y + props.values[0], y, props.scale, status)
+        })
       )
-    ]);
-  },
-  reducer: (action, state: State): State => {
-    switch (action.type) {
-      case "entered":
-        return {
-          ...state,
-          status: "entered"
-        };
-      case "update":
-        return {
-          ...state,
-          status: "exited",
-        };
-      case "show":
-        return {
-          ...state,
-          status: "entering",
-          values: state.nextValues,
-          scale: state.nextScale
-        };
-    }
+    );
   }
 });
