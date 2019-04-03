05f9f85b0fef63b825cbfeda53c60c199b6ef878
Vladimir Tolstikov <wertyxa@bk.ru>
2019-04-03 22:34:26 +0500

    fix scaling on safari

--- src/ruller.ts
+++ src/ruller.ts
@@ -4,26 +4,29 @@ import { TransitionGroup } from "./labels";
 import { Transition } from "./transition";
 
 const ruler = (projectY: number, y: number, scale: number, status: string) =>
-  createElement("g", { key: y }, [
-    createElement("line", {
-      x1: "0",
-      y1: projectY + "",
-      x2: "100%",
-      y2: projectY + "",
-      class: "r-line"
-    }),
-    createElement(
-      "text",
-      {
-        x: 0,
-        y: projectY - 5 + "",
-        class: `${status} transition translate r-text`,
-        style: `transform: scaleY(${1 /
-          scale}); transform-origin: 0 ${projectY - 5}px`
-      },
-      y.toString()
-    )
-  ]);
+  createElement(
+    "g",
+    {
+      key: y,
+      class: `${status} transition translate`,
+      style: `transform: scaleY(${1 / scale}); transform-origin: 0 ${projectY -
+        5}px`
+    },
+    [
+      createElement("line", {
+        x1: "0",
+        y1: projectY + "",
+        x2: "100%",
+        y2: projectY + "",
+        class: "r-line"
+      }),
+      createElement(
+        "text",
+        { x: 0, y: projectY - 5 + "", class: `r-text` },
+        y.toString()
+      )
+    ]
+  );
 
 export const TransitionRuller: ComponentType = () => ({
   ...componentMixin(),
