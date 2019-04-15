c751b3a370175b165b2a9fd4ba2109cf4923cb0b
Vladimir Tolstikov <wertyxa@bk.ru>
2019-04-16 01:28:50 +0500

    y2

--- src/axis.ts
+++ src/axis.ts
@@ -1,4 +1,4 @@
-import { CHART_WIDTH, PRECISION } from "./constant";
+import { CHART_WIDTH, CHART_HEIGHT } from "./constant";
 
 interface Bounds {
   scale: number;
@@ -13,22 +13,68 @@ interface Bounds {
   valueRange: number;
   values: number[];
 }
+const PRECISION = 8;
+const projectLength = function(axisLength, length, bounds) {
+  return (length / bounds.range) * axisLength;
+};
+
+
+const rho = function(num: number) {
+  if (num === 1) {
+    return num;
+  }
+
+  function gcd(p, q) {
+    if (p % q === 0) {
+      return q;
+    } else {
+      return gcd(q, p % q);
+    }
+  }
+
+  function f(x) {
+    return x * x + 1;
+  }
+
+  var x1 = 2,
+    x2 = 2,
+    divisor;
+  if (num % 2 === 0) {
+    return 2;
+  }
+
+  do {
+    x1 = f(x1) % num;
+    x2 = f(f(x2)) % num;
+    divisor = gcd(Math.abs(x1 - x2), num);
+  } while (divisor === 1);
+
+  return divisor;
+};
+
+export const round = function(value: number, digits?) {
+  var precision = Math.pow(10, digits || PRECISION);
+  return Math.round(value * precision) / precision;
+};
 
 const orderOfMagnitude = function(value) {
   return Math.floor(Math.log(Math.abs(value)) / Math.LN10);
 };
-
-export const round = function(value: number, digits: number) {
-  var precision = Math.pow(10, digits);
-  return Math.round(value * precision) / precision;
-};
-
-export const getBounds = function(_: number, high: number, low: number) {
-  var bounds = {
-    high,
-    low
-  } as Bounds;
-
+export const getBounds = function(
+  axisLength: number, 
+  high: number,
+  low: number,
+  scaleMinSpace = 50,
+  onlyInteger = false
+) {
+  var i,
+    optimizationCounter = 0,
+    newMin,
+    newMax,
+    bounds = {
+      high,
+      low
+    } as Bounds;
   bounds.valueRange = bounds.high - bounds.low;
   bounds.oom = orderOfMagnitude(bounds.valueRange);
   bounds.step = Math.pow(10, bounds.oom);
@@ -37,17 +83,89 @@ export const getBounds = function(_: number, high: number, low: number) {
   bounds.range = bounds.max - bounds.min;
   bounds.numberOfSteps = Math.round(bounds.range / bounds.step);
 
-  const step = bounds.range / 5;
-  const values2 = [];
-  for (let i = 0; i < 6; i++) {
-    values2.push(round(bounds.min + i * step,PRECISION));
+  // Optimize scale step by checking if subdivision is possible based on horizontalGridMinSpace
+  // If we are already below the scaleMinSpace value we will scale up
+  var length = projectLength(axisLength, bounds.step, bounds);
+  var scaleUp = length < scaleMinSpace;
+  var smallestFactor = onlyInteger ? rho(bounds.range) : 0;
+
+  // First check if we should only use integer steps and if step 1 is still larger than scaleMinSpace so we can use 1
+  if (onlyInteger && projectLength(axisLength, 1, bounds) >= scaleMinSpace) {
+    bounds.step = 1;
+  } else if (
+    onlyInteger &&
+    smallestFactor < bounds.step &&
+    projectLength(axisLength, smallestFactor, bounds) >= scaleMinSpace
+  ) {
+    // If step 1 was too small, we can try the smallest factor of range
+    // If the smallest factor is smaller than the current bounds.step and the projected length of smallest factor
+    // is larger than the scaleMinSpace we should go for it.
+    bounds.step = smallestFactor;
+  } else {
+    // Trying to divide or multiply by 2 and find the best step value
+    while (true) {
+      if (
+        scaleUp &&
+        projectLength(axisLength, bounds.step, bounds) <= scaleMinSpace
+      ) {
+        bounds.step *= 2;
+      } else if (
+        !scaleUp &&
+        projectLength(axisLength, bounds.step / 2, bounds) >= scaleMinSpace
+      ) {
+        bounds.step /= 2;
+        if (onlyInteger && bounds.step % 1 !== 0) {
+          bounds.step *= 2;
+          break;
+        }
+      } else {
+        break;
+      }
+
+      if (optimizationCounter++ > 1000) {
+        throw new Error(
+          "Exceeded maximum number of iterations while optimizing scale step!"
+        );
+      }
+    }
   }
-  bounds.values = values2;
+
+  var EPSILON = 2.221e-16;
+  bounds.step = Math.max(bounds.step, EPSILON);
+  function safeIncrement(value, increment) {
+    // If increment is too small use *= (1+EPSILON) as a simple nextafter
+    if (value === (value += increment)) {
+      value *= 1 + (increment > 0 ? EPSILON : -EPSILON);
+    }
+    return value;
+  }
+
+  // Narrow min and max based on new step
+  newMin = bounds.min;
+  newMax = bounds.max;
+  while (newMin + bounds.step <= bounds.low) {
+    newMin = safeIncrement(newMin, bounds.step);
+  }
+  while (newMax - bounds.step >= bounds.high) {
+    newMax = safeIncrement(newMax, -bounds.step);
+  }
+  bounds.min = newMin;
+  bounds.max = newMax;
+  bounds.range = bounds.max - bounds.min;
+
+  var values = [];
+  for (i = bounds.min; i <= bounds.max; i = safeIncrement(i, bounds.step)) {
+    var value = round(i);
+    if (value !== values[values.length - 1]) {
+      values.push(value);
+    };
+  }
+  bounds.values = values;  
   return bounds;
 };
 
 export const getBoundsX = (scale: number, high: number, low: number) => {
-  let step = (high - low + 3600000) / 7;
+  let step = (high - low) / 5;
   if (scale > 1.6) {
     step = step / 2;
   }
--- src/ruller.ts
+++ src/ruller.ts
@@ -16,15 +16,17 @@ export interface RullerProps {
 }
 
 interface State {
-    values: number[],
-    scaleDirection: 'up' | 'down',
-    transformOrigin: number
+  values: number[];
+  valuesY2: number[];
+  scaleDirection: "up" | "down";
+  transformOrigin: number;
 }
 
 export const TransitionRuller: ComponentType = () => ({
   ...componentMixin(),
   state: {
     values: null,
+    valuesY2: null,
     scaleDirection: null,
     transformOrigin: null
   } as State,
@@ -32,17 +34,37 @@ export const TransitionRuller: ComponentType = () => ({
     if (nextProps.valuesY2) return true;
     return !shallowEqual(nextProps.values, this.props.values);
   },
-  getDeriviedStateFromProps(props: RullerProps, prevState: State):State {
-    if (!props.values) return;
+  getDeriviedStateFromProps(props: RullerProps, prevState: State): State {
+    if (!props.values.length) {
+      if (!prevState.valuesY2) {
+        return { ...prevState, valuesY2: props.valuesY2 };
+      }
+      if (props.valuesY2.length) {
+        const range = last(props.valuesY2) - props.valuesY2[0];
+        const prevRange = last(prevState.valuesY2) - prevState.valuesY2[0];
+        if (prevRange !== range) {
+          const firstDiff = props.valuesY2[0] - prevState.valuesY2[0];
+          const lastDiff = last(props.valuesY2) - last(prevState.valuesY2);
+          const transformOrigin = firstDiff / (firstDiff + lastDiff);
+          return {
+            ...prevState,
+            valuesY2: props.valuesY2,
+            scaleDirection: prevRange > range ? "up" : "down",
+            transformOrigin
+          };
+        }
+      }
+      return prevState;
+    }
     if (!prevState.values) {
       return { ...prevState, values: props.values };
     }
     const range = last(props.values) - props.values[0];
     const prevRange = last(prevState.values) - prevState.values[0];
     if (prevRange !== range) {
-      const firstDiff = (props.values[0] - prevState.values[0])
-      const lastDiff = (last(props.values) - last(prevState.values))
-      const transformOrigin = firstDiff / (firstDiff + lastDiff)
+      const firstDiff = props.values[0] - prevState.values[0];
+      const lastDiff = last(props.values) - last(prevState.values);
+      const transformOrigin = firstDiff / (firstDiff + lastDiff);
       return {
         ...prevState,
         values: props.values,
@@ -55,6 +77,7 @@ export const TransitionRuller: ComponentType = () => ({
   render: (props: RullerProps, state: State) => {
     const { values, valuesY2, charts, pow } = props;
     const powToNumber = (v, pow) => {
+      if (v == null || isNaN(v)) return "";
       if (v === 0) return v + "";
       switch (pow) {
         case 1:
@@ -67,13 +90,28 @@ export const TransitionRuller: ComponentType = () => ({
     };
     let color1;
     let color2;
+    let valuesY2Intp = [];
     if (valuesY2) {
       const first = charts.find(c => c.id === "y0");
       const second = charts.find(c => c.id === "y1");
       color1 = first && first.color;
       color2 = second && second.color;
+      const range = last(valuesY2) - valuesY2[0];
+      const valuesLength = values.length || state.values.length;
+      if (valuesLength) {
+        const step = range / (valuesLength - 1);
+        // valuesY2Intp = values.map((_, i) => Math.round((valuesY2[0] + i * step) * 2) / 2);
+        for (let i = 0; i < valuesLength; i++ ){
+          valuesY2Intp.push(Math.round((valuesY2[0] + i * step) * 2) / 2)
+        }
+      } else {
+        valuesY2Intp = valuesY2;
+      }
     }
-    const key = `${values[0]}-${values[values.length - 1]}`;
+    const key =
+      !values.length && valuesY2
+        ? `${valuesY2[0]}-${last(valuesY2)}`
+        : `${values[0]}-${last(values)}`;
     return createElement(
       TransitionGroup,
       {
@@ -94,9 +132,10 @@ export const TransitionRuller: ComponentType = () => ({
                 {
                   key,
                   class: `${status} flex column abs fw fh`,
-                  style: `height: ${CHART_HEIGHT}px; transform-origin: 0 ${CHART_HEIGHT * (1 -  (passedProps && passedProps.t != null ? passedProps.t : state.transformOrigin))}px`
+                  //prettier-ignore
+                  style: `height: ${CHART_HEIGHT}px; transform-origin: 0 ${CHART_HEIGHT * (1 - (passedProps && passedProps.t != null ? passedProps.t : state.transformOrigin || 0))}px`
                 },
-                values.map((v, i) =>
+                (values.length ? values : valuesY2Intp).map((v, i) =>
                   createElement(
                     "div",
                     {
@@ -107,7 +146,7 @@ export const TransitionRuller: ComponentType = () => ({
                     ? powToNumber(v, pow)
                     : [
                         createElement("span", { style: `color: ${color1}`, class: `${!color1 ? 'exited' : 'entered'} transition` }, powToNumber(v, pow)),
-                        createElement("span", { style: `color: ${color2}`, class: `${!color2 ? 'exited' : 'entered'} transition`  }, powToNumber(valuesY2[i], pow))
+                        createElement("span", { style: `color: ${color2}`, class: `${!color2 ? 'exited' : 'entered'} transition`  }, powToNumber(valuesY2Intp[i], pow))
                       ]
                   )
                 )
